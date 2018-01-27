bee1343080f9553d920742165209f269924c0545
bonnieso <so_bonnie@hotmail.com>
2018-01-27 15:07:29 -0800

    Updating modules

--- bot.js
+++ bot.js
@@ -1,37 +1,38 @@
 var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
 
 var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+    token: "NDA2ODg1MDA5MTY1Nzc4OTU0.DU5c3w.kNYSyfs1tucTj3Lh6QSuMYbldUM",
+    autorun: true
 });
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+bot.on('ready', function() {
+    console.log('Logged in as %s - %s\n', bot.username, bot.id);
 });
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
+bot.on('message', function(user, userID, channelID, message, event) {
+  if (message.substring(0, 1) == '!') {
+      var args = message.substring(1).split(' ');
+      var cmd = args[0];
 
-        args = args.splice(1);
-        switch(cmd) {
-            case 'hello':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Hi there!'
-                });
-            break;
-         }
-     }
+      args = args.splice(1);
+      switch(cmd) {
+          case 'hello':
+              bot.sendMessage({
+                  to: channelID,
+                  message: 'Hi there!'
+              });
+          break;
+          case 'weed':
+              bot.sendMessage({
+                  to: channelID,
+                  message: 'Frequent usage of weed can have lead to idiocy!'
+              });
+          break;
+          case 'omg':
+            bot.joinVoiceChannel(channelID, function(error, events) {
+              console.log('joined voice', channelID);
+            });
+          break;
+       }
+   }
 });
